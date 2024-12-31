4fe5e32c8218371e943b4e12a2dc308e4cf3ef5c
Blackmamba <122316561+FantasticSukhi@users.noreply.github.com>
2025-01-01 01:12:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -48,9 +48,9 @@ async def startprivate(client, message):
     joinButton = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("CHANNEL", url="https://t.me/nacbots"),
+                InlineKeyboardButton("CHANNEL", url="https://t.me/MBV_NETWORK"),
                 InlineKeyboardButton(
-                    "SUPPORT GROUP", url="https://t.me/n_a_c_bot_developers"
+                    "SUPPORT GROUP", url="https://t.me/MBV_CHATS"
                 ),
             ]
         ]
